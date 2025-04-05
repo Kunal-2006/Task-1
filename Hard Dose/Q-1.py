1f37e13be32d1d1c3633a475fefd04a6d5bf40f3
Kunal-2006 <me24i1022@iiitdm.ac.in>
2025-04-06 04:49:28 +0530

    Update Q-1.py

--- Hard Dose/Q-1.py
+++ Hard Dose/Q-1.py
@@ -1,19 +1,55 @@
-# hard_dose_1.py
+file=open(
 
-# Size of the grid
-SIZE = 11
+R,C=#row and columns
+m=
+sr,sc=
+rq,cq=
 
-# Make an empty 11x11 grid with all 1s (safe spots)
-grid = []
-for i in range(SIZE):
-    row = []
-    for j in range(SIZE):
-        row.append(1)
-    grid.append(row)
+move_count=0
+nodes_ledt_in_layer=1
+nodes_in_next_layer=0
 
-file = open(file_name, "r")
-lines = file.readlines()
-file.close()
+reached_end =false
 
-
-input(int("enter the number of rows"))
+visited=
+dr=[-1,+1,0,0]
+dc=[0,0,+1,-1]
+def solve():
+    rq.enque(sr)
+    cq.enque(sc)
+    visited[sr][sc]=True
+    while rq.size>0:
+        r=rq.dequeue
+        c=cq.dequeue
+        if n[r][c]=='E':
+            reached_end=True
+            break
+            explore_neighbours(r,c)
+            nodes_left_in_laye--
+            if nodes_left_in_layer==0:
+                noded_left_in_layer==0:
+                noded_left_in_layer=nodes_in_next_layer
+                noded_in_next_layer=0
+                move_count++
+        if reached_end:
+            return move_count
+        return -1
+def explore_neighbours(r,c):
+    for(i in range[0,4]):
+        rr=r+dr[i]
+        cc=c+dc[i]
+    #skip out bounds position
+if rr<0 or cc<0:
+    continue
+if rr>=R or cc>=C:
+    continue
+    #skip the visited cells or vlovked cells
+if visited[rr][[cc]:
+    continue
+if m[rr][cc] =='#':
+    continue
+rq.enqueue(rr)
+cq.enqueue(cc)
+visited[rr][cc]= True
+nodes_in_next_layer++
+    
