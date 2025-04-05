129f9c707687800158b2ca6c57fc59b6a1012f31
Kunal-2006 <me24i1022@iiitdm.ac.in>
2025-04-06 04:55:31 +0530

    Update Q-1.py

--- Hard Dose/Q-1.py
+++ Hard Dose/Q-1.py
@@ -1,55 +1,67 @@
-file=open(
+from collections import deque
 
-R,C=#row and columns
-m=
-sr,sc=
-rq,cq=
+def read_grid_from_file(filename):
+    with open(filename, 'r') as file:
+        grid = [list(line.strip()) for line in file.readlines()]
+    return grid
 
-move_count=0
-nodes_ledt_in_layer=1
-nodes_in_next_layer=0
+def shortest_path(grid, start, end):
+    R, C = len(grid), len(grid[0])
+    visited = [[False for _ in range(C)] for _ in range(R)]
 
-reached_end =false
+    # Directions: up, down, right, left
+    dr = [-1, +1, 0, 0]
+    dc = [0, 0, +1, -1]
 
-visited=
-dr=[-1,+1,0,0]
-dc=[0,0,+1,-1]
-def solve():
-    rq.enque(sr)
-    cq.enque(sc)
-    visited[sr][sc]=True
-    while rq.size>0:
-        r=rq.dequeue
-        c=cq.dequeue
-        if n[r][c]=='E':
-            reached_end=True
+    rq = deque()
+    cq = deque()
+
+    sr, sc = start
+    rq.append(sr)
+    cq.append(sc)
+    visited[sr][sc] = True
+
+    move_count = 0
+    nodes_left_in_layer = 1
+    nodes_in_next_layer = 0
+    reached_end = False
+
+    while rq:
+        r = rq.popleft()
+        c = cq.popleft()
+
+        if (r, c) == end:
+            reached_end = True
             break
-            explore_neighbours(r,c)
-            nodes_left_in_laye--
-            if nodes_left_in_layer==0:
-                noded_left_in_layer==0:
-                noded_left_in_layer=nodes_in_next_layer
-                noded_in_next_layer=0
-                move_count++
-        if reached_end:
-            return move_count
-        return -1
-def explore_neighbours(r,c):
-    for(i in range[0,4]):
-        rr=r+dr[i]
-        cc=c+dc[i]
-    #skip out bounds position
-if rr<0 or cc<0:
-    continue
-if rr>=R or cc>=C:
-    continue
-    #skip the visited cells or vlovked cells
-if visited[rr][[cc]:
-    continue
-if m[rr][cc] =='#':
-    continue
-rq.enqueue(rr)
-cq.enqueue(cc)
-visited[rr][cc]= True
-nodes_in_next_layer++
-    
+
+        # Explore neighbors
+        for i in range(4):
+            rr = r + dr[i]
+            cc = c + dc[i]
+
+            if rr < 0 or cc < 0 or rr >= R or cc >= C:
+                continue
+            if visited[rr][cc]:
+                continue
+            if grid[rr][cc] == '#':
+                continue
+
+            rq.append(rr)
+            cq.append(cc)
+            visited[rr][cc] = True
+            nodes_in_next_layer += 1
+
+        nodes_left_in_layer -= 1
+        if nodes_left_in_layer == 0:
+            nodes_left_in_layer = nodes_in_next_layer
+            nodes_in_next_layer = 0
+            move_count += 1
+
+    return move_count if reached_end else -1
+
+# === MAIN ===
+grid = read_grid_from_file("log.txt")
+start = (0, 0)
+end = (10, 10)
+result = shortest_path(grid, start, end)
+print("Shortest path from (0,0) to (10,10):", result)
