7efbf38a80eb4ea410ca6aaac846f46acf5952a9
Kunal-2006 <me24i1022@iiitdm.ac.in>
2025-04-06 04:21:14 +0530

    Update Q-1.py

--- Hard Dose/Q-1.py
+++ Hard Dose/Q-1.py
@@ -1,5 +1,19 @@
-def explore neighbours(r,c):
-  for(i in range[0,4]):
-    rr=r+dr[i]
-    cc=c+dc[i]
-    
+# hard_dose_1.py
+
+# Size of the grid
+SIZE = 11
+
+# Make an empty 11x11 grid with all 1s (safe spots)
+grid = []
+for i in range(SIZE):
+    row = []
+    for j in range(SIZE):
+        row.append(1)
+    grid.append(row)
+
+file = open(file_name, "r")
+lines = file.readlines()
+file.close()
+
+
+input(int("enter the number of rows"))
